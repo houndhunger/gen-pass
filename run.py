e71e69337da220b722a7afc7d74414ca090bc344
Daniel Pribula <daniel.pribula@gmail.com>
2024-04-19 00:49:28 +0000

    end message edit

--- run.py
+++ run.py
@@ -1,15 +1,9 @@
-
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-
-
-
-"""
-checks if xsel is installed for clypbosrd op
-"""
-
 import subprocess
 import platform
 def is_xsel_installed():
+    """
+    checks if xsel is installed for clypbosrd op
+    """
     if platform.system() != 'Linux':
         return False  # xsel is only available on Linux
     try:
@@ -25,54 +19,21 @@ def is_pyperclip_installed():
         return True
     except ImportError:
         return False
-  
-##### operation_status() not needed - not used
-def operation_status(operation):
-    status = f"Operation status:"
-    # Check if 'value' key exists in the operation dictionary
-    if 'value' in operation:
-        status += f" {operation['name']} <{operation['value']}>,"  
-        # Check if 'value' is 'No' and return status if it is
-        if operation['value'] == 'No':
-            return status
-    # Add 'Min' and 'Max' to the status
-    status += f" Min: <{operation.get('min', '')}>, Max: <{operation.get('max', '')}>."
-    return status
-    
-##### operation_section() not needed - not used
-def operation_section(operation):
-    #Operation Name - Header
-    section = f"* {operation['name']} *\n"
-    #Operation status
-    section += f"{operation_status(operation)}\n"
-    return section    
-
-##### input_valid() not needed - not used
-def input_valid(inp_value):
-    """
-    Checks if user input is valid.
-    Returns true if it is, if not it will return message
-    """
-    if inp_value in ('L', 'U', 'O', 'N', 'S', 'G', 'E'):
-        return True, ""
-    else: ##### is this used? because input varries
-        message = ("Input value is invalid. \n"
-        "Type in 'L' 'U', 'O', 'N', 'S', 'G', 'E' \n"
-        "or 'l' 'u', 'o', 'n', 's', 'g', 'e'.")
-        return False, message
-
+ 
 import calculations
-#import sections
 import validate
 def end():
+    """
+    End of script
+    """
         print(f"\n*** \nEnding Password Generator. " +\
-        f"\nMemory has been cleared. \nStay safe and Goodbye.")
+        f"\nStay safe and Goodbye.")
 
 def main():
     """
-    Main function of the program which sets settings, 
+    Main function of the script which sets settings, 
     starts main screen, where whole process goes
-    and ends program.
+    and ends script.
     """
     settings = {
     # Password Length
@@ -92,7 +53,7 @@ def main():
     # Holds active operation to display crelevant content i.e. key operations
     'ACTIVE-OP': 'HOME'
     }
-    #main screen of the program
+    #main screen of the script
     validate.screen_and_get_operation(settings)
     end()
 
